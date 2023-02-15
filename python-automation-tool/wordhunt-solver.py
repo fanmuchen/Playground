8b04f9d0c1143c7cd3d5a92d5e20600bf7d98c39
fanmuchen <fmc222813@gmail.com>
2023-02-16 03:04:16 +0800

    update wordhunt solver.

--- python-automation-tool/wordhunt-solver.py
+++ python-automation-tool/wordhunt-solver.py
@@ -1,37 +1,58 @@
 # Define the 4x4 grid of letters
 letters = [
-    ['A', 'B', 'C', 'D'],
-    ['E', 'F', 'G', 'H'],
-    ['I', 'J', 'K', 'L'],
-    ['M', 'N', 'O', 'P']
+    ['S', 'S', 'E', 'R'],
+    ['U', 'H', 'O', 'T'],
+    ['I', 'E', 'N', 'L'],
+    ['R', 'B', 'T', 'H']
 ]
 
+# Ask the user to enter the letters as a string
+# letters_str = input("Enter the 16 letters as a single string: ").upper()
+
+# # Convert the string to a 4x4 grid of letters
+# letters = [[letters_str[i*4 + j] for j in range(4)] for i in range(4)]
+
+# Read the English dictionary file into a set
+dictionary = set()
+with open('files/Collins Scrabble Words (2019).txt', 'r') as f:
+    for line in f:
+        dictionary.add(line.strip().upper())
+
 # Define a list of words to search for
-words = ['ABC', 'DEF', 'GHI', 'MNO', 'ADG',
-         'BEH', 'CFI', 'MGL', 'NHL', 'KJ', "MJGDHLPKFA"]
+# words = ['ABC', 'DEF', 'GHI', 'MNO', 'ADG',
+#          'BEH', 'CFI', 'MGL', 'NHL', 'KJ', 'MJGKFA']
+words = dictionary
 
 # Define a function to check if a word can be formed in the grid
 
 
 def is_word_possible(word, grid):
-    # Check if the word can be formed horizontally
-    for row in grid:
-        if word in ''.join(row):
-            return True
-    # Check if the word can be formed vertically
-    for col in range(len(grid[0])):
-        if word in ''.join([row[col] for row in grid]):
-            return True
-    # Check if the word can be formed diagonally
+    # Check all starting positions for the first letter of the word
     for i in range(len(grid)):
-        if word in ''.join([grid[i+j][j] for j in range(min(len(grid)-i, len(grid[0])))]):
-            return True
-        if word in ''.join([grid[j][i+j] for j in range(min(len(grid[0])-i, len(grid)))]):
-            return True
-        if word in ''.join([grid[i-j][j] for j in range(min(i+1, len(grid[0])))]):
-            return True
-        if word in ''.join([grid[j][i-j] for j in range(min(len(grid)-i, len(grid[0])))]):
-            return True
+        for j in range(len(grid[0])):
+            if word[0] == grid[i][j]:
+                # Check all possible paths from the starting position
+                if is_word_possible_helper(word[1:], grid, [(i, j)]):
+                    return True
+    return False
+
+# Define a helper function to check if a word can be formed in the grid from a given position
+
+
+def is_word_possible_helper(word, grid, path):
+    if len(word) == 0:
+        return True
+    # Get the current position from the path
+    i, j = path[-1]
+    # Check all adjacent positions for the next letter of the word
+    for di in range(-1, 2):
+        for dj in range(-1, 2):
+            # Ignore the current position and out-of-bounds positions
+            if (di, dj) != (0, 0) and 0 <= i+di < len(grid) and 0 <= j+dj < len(grid[0]):
+                if word[0] == grid[i+di][j+dj] and (i+di, j+dj) not in path:
+                    # Recursively check the next letter of the word
+                    if is_word_possible_helper(word[1:], grid, path + [(i+di, j+dj)]):
+                        return True
     return False
 
 
@@ -43,8 +64,8 @@ for word in words:
     if is_word_possible(word, letters):
         found_words.append(word)
 
-# Sort the list of found words by length (from longest to shortest)
-found_words.sort(key=len, reverse=True)
+# Sort the list of found words by length (from longest to shortest), then alphabetically
+found_words.sort(key=lambda x: (-len(x), x))
 
 # Print the list of found words
 print(found_words)
